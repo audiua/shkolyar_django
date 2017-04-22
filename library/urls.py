e826a91a7c86b26cada1b85c2e05b0ceb9818cad
Kostia Tarnashynskyi <kostia.tarnashynskyi@gmail.com>
2017-04-22 15:04:38 +0300

    top articles, fix urls

--- library/urls.py
+++ library/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<author_slug>[-a-z0-9]+)/(?P<book_slug>[-a-z0-9]+)$',
+    url(r'^/(?P<author_slug>[-a-z0-9]+)/(?P<book_slug>[-a-z0-9]+)$',
         views.book, name='book'),
     url(r'^(?P<author_slug>[-a-z0-9]+)$', views.author, name='author'),
 
-    url(r'^page/(?P<page>\d+)$', views.index, name='index_paginate'),
+    url(r'^/page/(?P<page>\d+)$', views.index, name='index_paginate'),
     url(r'^$', views.index, name='index'),
 ]
